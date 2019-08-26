10c5a7ce2554e3beb7e0e1c6aa77a56f5fe3b0ac
industrious-modal-4420 <khaledhassan45@gmail.com>
2019-08-26 20:33:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word(sentence)
-  new_array = sentence.split.collect{|word| 
-    word.reverse!}
+  new_array = sentence.split.each{|word| word.reverse!}
   new_array.join(' ')
 end
   
